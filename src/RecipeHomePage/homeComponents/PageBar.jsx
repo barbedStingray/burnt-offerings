78411e77ce67104ae4370ec51a711c39d74c48b2
Ben Rehmann <brehmann@me.com>
2024-10-11 23:03:29 -0500

    scrolling

--- src/RecipeHomePage/homeComponents/PageBar.jsx
+++ src/RecipeHomePage/homeComponents/PageBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowDropright } from "react-icons/io";
 import { IoIosArrowDropleft } from "react-icons/io";
 
@@ -6,19 +6,14 @@ import { IoIosArrowDropleft } from "react-icons/io";
 const PageBar = ({ pageStatus }) => {
     const { currentPage, setCurrentPage, totalPages, scrollToTopRef } = pageStatus
 
-
     const handlePageChange = (newPage) => {
             setCurrentPage(newPage)
 
             setTimeout(() => {
                 if (scrollToTopRef.current) {
-                    const scrollHeight = scrollToTopRef.current.scrollHeight
-                    const clientHeight = scrollToTopRef.current.clientHeight
-                    if (scrollHeight > clientHeight) {
-                        scrollToTopRef.current.scrollTo({ top: 0, behavior: 'smooth' })
-                    }
+                    scrollToTopRef.current.scrollTo({ top: 0, behavior: 'smooth' })
                 }
-            }, 500)
+            }, 200)
     }
 
 
